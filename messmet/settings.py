2be06ea9ebe4de4a57f660eb2da92985ba75baec
gangeshwar01 <gangeshwarkumar001@gmail.com>
2025-10-03 15:56:17 +0530

    postgress

--- messmet/settings.py
+++ messmet/settings.py
@@ -82,7 +82,11 @@ WSGI_APPLICATION = 'messmet.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}", conn_max_age=600)
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        conn_max_age=600,
+        ssl_require=not DEBUG,
+    )
 }
 
 
